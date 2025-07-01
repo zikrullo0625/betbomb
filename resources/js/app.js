ba34925fcd01c2f67fed491fe69584056f55b854
Zikrullo <zikrullogaming@gmail.com>
2025-07-01 22:52:40 +0500

    add vuejs to project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import { createApp } from 'vue'
+
+// import Home from './components/Home.vue'
+
+const app = createApp({})
+
+// app.component('home', Home)
+
+app.mount('#app')
